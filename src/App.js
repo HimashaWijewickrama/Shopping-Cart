9aeb3e3c8135f4c305323ac1cd8122c719e653aa
myusername <himashawije5678@gmail.com>
2024-01-28 23:03:18 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,23 +7,33 @@ import UserCartComponent from './components/UserCartComponent';
 
 function App() {
 	const [courses, setCourses] = useState([
-		{ id: 1, 
-		name: 'Shoes', 
-		price: 499, 
-		image: 
-'https://images.unsplash.com/photo-1499013819532-e4ff41b00669?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		{
+			id: 1,
+			name: 'Shoes',
+			price: 499,
+			image:
+				'https://images.unsplash.com/photo-1499013819532-e4ff41b00669?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 		},
-		{ id: 2, 
-		name: 'Bag', 
-		price: 699, 
-		image: 
-'https://images.unsplash.com/photo-1606522754091-a3bbf9ad4cb3?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		{
+			id: 2,
+			name: 'Bag',
+			price: 699,
+			image:
+				'https://images.unsplash.com/photo-1606522754091-a3bbf9ad4cb3?q=80&w=2042&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 		},
-		{ id: 3, 
-		name: 'Sneaker', 
-		price: 799, 
-		image: 
-'https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		{
+			id: 3,
+			name: 'Sneaker',
+			price: 799,
+			image:
+				'https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+		},
+		{
+			id: 4,
+			name: 'Sunglass',
+			price: 980,
+			image:
+				'https://images.unsplash.com/photo-1523754865311-b886113bb8de?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 		}
 	]);
 
@@ -32,29 +42,30 @@ function App() {
 
 	const addCourseToCartFunction = (GFGcourse) => {
 		const alreadyCourses = cartCourses
-							.find(item => item.product.id === GFGcourse.id);
+			.find(item => item.product.id === GFGcourse.id);
 		if (alreadyCourses) {
 			const latestCartUpdate = cartCourses.map(item =>
-				item.product.id === GFGcourse.id ? { 
-				...item, quantity: item.quantity + 1 } 
-				: item
+				item.product.id === GFGcourse.id ? {
+					...item, quantity: item.quantity + 1
+				}
+					: item
 			);
 			setCartCourses(latestCartUpdate);
 		} else {
-			setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+			setCartCourses([...cartCourses, { product: GFGcourse, quantity: 1 }]);
 		}
 	};
 
 	const deleteCourseFromCartFunction = (GFGCourse) => {
 		const updatedCart = cartCourses
-							.filter(item => item.product.id !== GFGCourse.id);
+			.filter(item => item.product.id !== GFGCourse.id);
 		setCartCourses(updatedCart);
 	};
 
 	const totalAmountCalculationFunction = () => {
 		return cartCourses
-			.reduce((total, item) => 
-						total + item.product.price * item.quantity, 0);
+			.reduce((total, item) =>
+				total + item.product.price * item.quantity, 0);
 	};
 
 	const courseSearchUserFunction = (event) => {
@@ -67,10 +78,10 @@ function App() {
 
 	return (
 		<div className="App">
-			<SearchComponent searchCourse={searchCourse} 
-							courseSearchUserFunction=
-								{courseSearchUserFunction} />
-			<main className="App-main">
+			<SearchComponent searchCourse={searchCourse}
+				courseSearchUserFunction=
+				{courseSearchUserFunction} />
+			<main className="App-main1">
 				<ShowCourseComponent
 					courses={courses}
 					filterCourseFunction={filterCourseFunction}
@@ -86,6 +97,7 @@ function App() {
 					setCartCourses={setCartCourses}
 				/>
 			</main>
+
 		</div>
 	);
 }
--- src/components/ShowCourseComponent.js
+++ src/components/ShowCourseComponent.js
@@ -1,29 +1,31 @@
 import React from 'react';
 
-function ShowCourseComponent({ courses, 
-	filterCourseFunction, 
+function ShowCourseComponent({ courses,
+	filterCourseFunction,
 	addCourseToCartFunction }) {
 	return (
-		<div className="product-list">
-			{filterCourseFunction.length === 0 ? (
-				<p className="no-results">
-					Sorry, No matching Product found.
-				</p>
-			) : (
-				filterCourseFunction.map((product) => (
-					<div className="product" key={product.id}>
-						<img src={product.image} alt={product.name} />
-						<h2>{product.name}</h2>
-						<p>Price: Rs.{product.price}</p>
-						<button
-							className="add-to-cart-button"
-							onClick={() => addCourseToCartFunction(product)}
-						>
-							Add to Shopping Cart
-						</button>
-					</div>
-				))
-			)}
+		<div className='products'>
+			<div className="product-list">
+				{filterCourseFunction.length === 0 ? (
+					<p className="no-results">
+						Sorry, No matching Product found.
+					</p>
+				) : (
+					filterCourseFunction.map((product) => (
+						<div className="product" key={product.id}>
+							<img src={product.image} alt={product.name} />
+							<h2>{product.name}</h2>
+							<p>Price: Rs.{product.price}</p>
+							<button
+								className="add-to-cart-button"
+								onClick={() => addCourseToCartFunction(product)}
+							>
+								Add to Shopping Cart
+							</button>
+						</div>
+					))
+				)}
+			</div>
 		</div>
 	);
 }
